1caa76940a96f3c85fba3b5eb3e2a96b73ff5681
FeliciaPompei <f.pompei@hotmail.com>
2022-03-28 15:46:03 +0200

    folders, file and img added, header and main positioned

---
+++ src/components/IndexHeader.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="main-header">
+    <div id="header-wrapper">
+      <img src="../assets/img/dc-logo.png" alt="DC logo">
+      <ul>
+        <li>
+          <a href="#">characters</a>
+        </li>
+        <li>
+          <a href="#">comics</a>
+        </li>
+        <li>
+          <a href="#">movies</a>
+        </li>
+        <li>
+          <a href="#">tv</a>
+        </li>
+        <li>
+          <a href="#">games</a>
+        </li>
+        <li>
+          <a href="#">collectibles</a>
+        </li>
+        <li>
+          <a href="#">videos</a>
+        </li>
+        <li>
+          <a href="#">fans</a>
+        </li>
+        <li>
+          <a href="#">news</a>
+        </li>
+        <li>
+          <a href="#">shop</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexHeader',
+}
+</script>
+
+
+<style scoped lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#header-wrapper{
+  display: flex;
+  justify-content: space-between;
+  align-items:center;
+  height: 20vh;
+  width: 70%;
+  margin: 0 auto;
+
+  img{
+    height:55%;
+  }
+
+  li{
+    list-style: none;
+    display: inline;
+    padding: 1rem;
+    font-size: 0.7rem;
+
+
+    a{
+      text-decoration: none;
+      text-transform: uppercase;
+      color:black;
+    }
+  }
+}
+
+
+</style>
---
+++ src/components/IndexMain.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="bg-gray">
+      <div class="main-content">
+        <h1> Content goes here </h1>
+    </div>
+    <div class="bg-blue">
+      <div class="main-content">
+        <ul>
+          <li>
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="DC digital comics">
+            <a href="#">digital comics</a>
+          </li>
+          <li>
+            <img src="../assets/img/buy-comics-merchandise.png" alt="DC merchandise">
+            <a href="#">dc merchamdise</a>
+          </li>
+          <li>
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="DC subscriptions">
+            <a href="#">subscription</a>
+          </li>
+          <li>
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="DC digital shop locator">
+            <a href="#">comic shop locator</a>
+          </li>
+          <li>
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="DC power visa">
+            <a href="#">dc power visa</a>
+          </li>
+        </ul>
+    </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexMain',
+}
+</script>
+
+
+<style scoped lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.bg-gray{
+  background-color:#1c1c1c;
+}
+
+.main-content{
+  display: flex;
+  align-items: center;
+  height: 20vh;
+  width: 70%;
+  margin: 0 auto;
+
+  h1 {
+    color:white;
+  }
+
+  li{
+    list-style: none;
+    display: inline;
+    margin:1rem;
+
+  img{
+    width:5%;
+    vertical-align: middle;
+    margin-right: 0.4rem;
+    
+  }
+
+    a {
+      text-decoration: none;
+      text-transform: uppercase;
+      color:white;
+      font-size: 0.5rem;
+    }
+  }
+}
+
+.bg-blue{
+  background-color:#0282f9;
+}
+
+</style>
